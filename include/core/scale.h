1c8c0eca2482dc28ce1a0ddaaea90d8a9ef117cb
Andrew Orals <nd-0r@andreworals.com>
2021-04-20 01:27:27 -0500

    Add constructor to Scale header

--- include/core/scale.h
+++ include/core/scale.h
@@ -3,26 +3,33 @@
 
 #include <vector>
 #include <string>
+#include <cmath>
 
 namespace scalepiegraph {
 
 class Scale {
  public:
-  void UpdateIntervalSize(size_t interval_num, float percent_change);
+  Scale(const std::string& name, const std::vector<float>& intervals);
 
-  float CalculateNoteFrequency(float base_freq, size_t note_index) const;
+  void UpdateIntervalSize(size_t inter_index, float percent_change);
 
-  float GetInterval(size_t inter_num) const;
+  double CalculateNoteFrequency(float base_freq, size_t note_index) const;
+
+  float GetInterval(size_t inter_index) const;
 
   size_t GetNumIntervals() const;
 
   size_t GetNumNotes() const;
+
+  const std::string& GetName() const;
 
   bool operator==(const Scale& other_scale) const;
 
   bool operator!=(const Scale& other_scale) const;
 
  private:
+  const float kCentsInOctave = 1200.0;
+
   std::string name_;
   std::vector<float> intervals_;
 
