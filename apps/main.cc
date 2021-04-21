fb2217c850aa8133c79041360ba8cb22e8b72ceb
Andrew Orals <nd-0r@andreworals.com>
2021-04-20 22:24:52 -0500

    Implement library demonstration in main

--- apps/main.cc
+++ apps/main.cc
@@ -1,22 +1,58 @@
 #include <core/synthesizer.h>
+#include <core/scale.h>
 #include <unistd.h>
+#include <iostream>
 #include <jsoncpp/json.h>
+#include <fstream>
 
-int main() {
-  scalepiegraph::Synthesizer synthesizer;
-  synthesizer.Play(440.0);
-  synthesizer.SetFilter(440.0);
-
+void showcase_synthesizer() {
   const size_t kSecond = 1000000;
-  usleep(1 * kSecond);
+  const float kBaseFrequency = 440.0;
+
+  scalepiegraph::Synthesizer synthesizer;
+  scalepiegraph::Scale scale(12);
+
+  synthesizer.Start(kBaseFrequency);
+  synthesizer.SetFilter(kBaseFrequency);
 
   synthesizer.SetWaveform(ci::audio::WaveformType::SAWTOOTH);
   usleep(kSecond);
+  synthesizer.SetFrequency(scale.CalculateNoteFrequency(kBaseFrequency, 2));
   synthesizer.SetWaveform(ci::audio::WaveformType::SQUARE);
   usleep(kSecond);
+  synthesizer.SetFrequency(scale.CalculateNoteFrequency(kBaseFrequency, 4));
   synthesizer.SetWaveform(ci::audio::WaveformType::TRIANGLE);
   usleep(kSecond);
 
   synthesizer.Stop();
+}
+
+void showcase_json_library(std::ifstream& json_file) {
+
+  Json::Value root;
+  json_file >> root;
+
+  std::cout << root.get("encoding", "UTF-8" ).asString() << std::endl;
+
+  const Json::Value plugins = root["plug-ins"];
+
+  for (const Json::Value& plugin : plugins) {
+    std::cout << plugin.asString() << std::endl;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  std::ifstream test_json_file;
+
+  try {
+    test_json_file.open(argv[1]);
+  } catch (std::ios_base::failure&) {
+    std::cerr << "Cannot open provided files";
+    return 1;
+  }
+
+  showcase_json_library(test_json_file);
+  showcase_synthesizer();
+
   return 0;
 }
