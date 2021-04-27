f43195e6035734ae51dd1297bf5c0d2dd3e7a3d6
Andrew Orals <nd-0r@andreworals.com>
2021-04-27 12:57:01 -0500

    Stub out pie graph header

--- include/frontend/pie_graph.h
+++ include/frontend/pie_graph.h
@@ -1,8 +1,43 @@
-//
-// Created by Andrew Orals on 4/26/21.
-//
+// Copyright (c) 2021 Andrew Orals. All rights reserved.
+#pragma once
 
-#ifndef SCALE_PIE_GRAPH_PIE_GRAPH_H
-#define SCALE_PIE_GRAPH_PIE_GRAPH_H
+#include <cstdlib>
+#include <glm/detail/type_vec2.hpp>
+#include <glm/vec2.hpp>
+#include <cinder/Path2d.h>
+#include <cinder/Shape2d.h>
+#include <cinder/gl/draw.h>
+#include <cinder/gl/wrapper.h>
 
-#endif //SCALE_PIE_GRAPH_PIE_GRAPH_H
+namespace scalepiegraph {
+
+namespace frontend {
+
+class PieGraph {
+ public:
+  PieGraph() = default;
+
+  PieGraph(const glm::vec2& pos, float radius, std::vector<float> proportions);
+
+  void Draw();
+
+  size_t GetNearestHandleIndex(const glm::vec2& pos) const;
+
+  void UpdateHandle(size_t handle_index, glm::vec2& new_pos);
+
+  std::vector<float> GetProportions() const;
+
+ private:
+  static const float kCircleStartOffset;
+  static const float kHandleRadius;
+  static const ci::Color kStrokeColor;
+
+  glm::vec2 center_;
+  float radius_;
+  std::vector<float> proportions_;
+  std::vector<glm::vec2> handles_;
+};
+
+}
+
+}
