c4975c45b807ae17d5787338e54b9b9ddd195d3b
Andrew Orals <nd-0r@andreworals.com>
2021-04-27 12:58:57 -0500

    Implement basic app-wide drawing

--- src/frontend/scale_pie_graph_app.cc
+++ src/frontend/scale_pie_graph_app.cc
@@ -1,1 +1,43 @@
-#include <core/scale.h>
+// Copyright (c) 2021 Andrew Orals. All rights reserved.
+#include <frontend/scale_pie_graph_app.h>
+
+namespace scalepiegraph {
+
+namespace frontend {
+
+ScalePieGraphApp::ScalePieGraphApp() :
+    current_width_(kDefaultWindowSize),
+    current_height_(kDefaultWindowSize),
+    current_scale_(Scale(12)) {
+  ci::app::setWindowSize(current_width_, current_height_);
+
+  glm::vec2 graph_center(current_width_ / 2, current_height_ / 3);
+  graph_ = PieGraph(graph_center,
+                    graph_center.y - kDefaultMargin,
+                    current_scale_.GetProportions());
+
+  keyboard_ = Keyboard(glm::vec2(0, current_height_),
+                       current_width_,
+                       current_height_ / 3,
+                       current_scale_.GetNumIntervals());
+
+  scale_dataset_ = ScaleDataset();
+}
+
+void ScalePieGraphApp::draw() {
+  ci::gl::clear(kBackgroundColor);
+  graph_.Draw();
+  keyboard_.Draw();
+}
+
+// void mouseDown(ci::app::MouseEvent event);
+
+//void mouseDrag(ci::app::MouseEvent event);
+//
+//void keyDown(ci::app::KeyEvent event);
+//
+//void fileDrop(ci::app::FileDropEvent event);
+
+}
+
+}
