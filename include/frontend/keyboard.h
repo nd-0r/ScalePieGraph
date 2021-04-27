ff9527c0fa340e559df7cb046b22c1a1df3c075e
Andrew Orals <nd-0r@andreworals.com>
2021-04-27 12:58:07 -0500

    Stub out keyboard header

--- include/frontend/keyboard.h
+++ include/frontend/keyboard.h
@@ -1,11 +1,31 @@
 // Copyright (c) 2021 Andrew Orals. All rights reserved.
 #pragma once
 
+#include <cinder/gl/gl.h>
+
 namespace scalepiegraph {
 
 namespace frontend {
 
+class Keyboard {
+ public:
+  Keyboard() = default;
 
+  Keyboard(const glm::vec2& bottom_left_corner,
+           float width,
+           float height,
+           size_t num_divisions);
+
+  void Draw();
+
+  void UpdateDivisions(size_t num_divisions);
+
+ private:
+  glm::vec2 bottom_left_corner_;
+  float width_;
+  float height_;
+  size_t current_divisions_;
+};
 
 }
 
