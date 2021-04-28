d6f3d07e4ea0d08893eedce87bc88c198256bfcd
Andrew Orals <nd-0r@andreworals.com>
2021-04-27 22:24:45 -0500

    Implement mouse up and down in cinder app

--- src/frontend/scale_pie_graph_app.cc
+++ src/frontend/scale_pie_graph_app.cc
@@ -30,10 +30,40 @@ void ScalePieGraphApp::draw() {
   keyboard_.Draw();
 }
 
-// void mouseDown(ci::app::MouseEvent event);
+void ScalePieGraphApp::mouseDown(ci::app::MouseEvent event) {
+  last_mouse_down_pos = event.getPos();
+  std::cout << last_mouse_down_pos << std::endl;
+}
 
-//void mouseDrag(ci::app::MouseEvent event);
-//
+void ScalePieGraphApp::mouseUp(ci::app::MouseEvent event) {
+  float window_x_pos = event.getWindow()->getPos().x;
+  float window_y_pos = event.getWindow()->getPos().y;
+  glm::vec2 mouse_pos = glm::vec2(
+      event.getPos().x - window_x_pos, event.getPos().y - window_y_pos);
+  std::cout << mouse_pos << std::endl;
+
+  if (glm::distance(mouse_pos, last_mouse_down_pos) > 1) {
+    int handle_idx = graph_.GetHandleIndex(event.getPos());
+    std::cout << handle_idx << std::endl;
+    if (graph_.UpdateHandle(handle_idx, mouse_pos)) {
+      std::cout << "good" << std::endl;
+    }
+    std::cout << "bad" << std::endl;
+  }
+}
+// TODO - fix
+/*void ScalePieGraphApp::mouseDrag(ci::app::MouseEvent event) {
+  glm::vec2 mouse_pos = event.getPos();
+
+  if (glm::distance(mouse_pos, last_mouse_down_pos) > 1) {
+    int handle_idx = graph_.GetHandleIndex(mouse_pos);
+    if (graph_.UpdateHandle(handle_idx, mouse_pos)) {
+      std::cout << "good" << std::endl;
+    }
+  }
+}*/
+
+// TODO - implement
 //void keyDown(ci::app::KeyEvent event);
 //
 //void fileDrop(ci::app::FileDropEvent event);
