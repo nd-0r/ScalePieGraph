19d670e430a16b07f523c9da8d2a5617b40e5876
Andrew Orals <nd-0r@andreworals.com>
2021-04-27 22:19:34 -0500

    Implement draw and create handles

--- src/frontend/pie_graph.cc
+++ src/frontend/pie_graph.cc
@@ -20,6 +20,54 @@ PieGraph::PieGraph(
   CreateHandles(false);
 }
 
+void PieGraph::Draw() {
+  ci::Path2d outer_arc;
+  ci::Path2d end_caps;
+
+  float arc_end = division_radians_.back() + kCircleStartOffset;
+  outer_arc.arc(center_, radius_, kCircleStartOffset, -arc_end, false);
+  outer_arc.lineTo(center_);
+
+  glm::vec2 start(center_.x, center_.y - radius_);
+  end_caps.moveTo(center_);
+  end_caps.lineTo(start);
+
+  ci::gl::color(kStrokeColor);
+  ci::gl::draw(outer_arc);
+  ci::gl::draw(end_caps);
+
+  current_arc_ = outer_arc;
+
+  CreateHandles(true);
+}
+
+
+
+void PieGraph::CreateHandles(bool should_draw) {
+  current_handles_ = std::vector<ci::Path2d>();
+
+  glm::vec2 rad_vec = glm::vec2(center_.x, center_.y - radius_) - center_;
+
+  for (float sweep : division_radians_) {
+    sweep += kCircleStartOffset;
+
+    float new_x = glm::cos(sweep) * rad_vec.x + glm::sin(sweep) * rad_vec.y;
+    float new_y = -glm::sin(sweep) * rad_vec.x + glm::cos(sweep) * rad_vec.y;
+    glm::vec2 handle_point = glm::vec2(new_x + center_.x, new_y + center_.y);
+
+    ci::Path2d handle;
+    handle.arc(handle_point, kHandleRadius, 0, 2 * glm::pi<float>());
+    handle.close();
+    current_handles_.push_back(handle);
+
+    if (should_draw) {
+      ci::gl::color(kStrokeColor);
+      ci::gl::drawLine(center_, handle_point);
+      ci::gl::drawSolid(handle);
+    }
+  }
+}
+
 } // namespace frontend
 
 } // namespace scalepiegraph
