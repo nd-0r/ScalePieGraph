6b45a699b3e0d880d16b778129b2342e26f0113b
Andrew Orals <nd-0r@andreworals.com>
2021-04-27 22:29:05 -0500

    Add documentation to keyboard

--- include/frontend/keyboard.h
+++ include/frontend/keyboard.h
@@ -7,17 +7,41 @@ namespace scalepiegraph {
 
 namespace frontend {
 
+/**
+ * A class representing a keyboard to use as onscreen input to
+ * trigger a synthesizer.
+ */
 class Keyboard {
  public:
+  /**
+   * Construct a keyboard with all default values. Cannot be drawn.
+   */
   Keyboard() = default;
 
+  /**
+   * Construct a Keyboard in the specified location with the specified width,
+   * height, and number of divisions.
+   *
+   * @param bottom_left_corner The location of the bottom left corner
+   * @param width The width of the keyboard
+   * @param height The height of the keyboard
+   * @param num_divisions The number of divisions of the keyboard
+   */
   Keyboard(const glm::vec2& bottom_left_corner,
            float width,
            float height,
            size_t num_divisions);
 
+  /**
+   * Draw this keyboard.
+   */
   void Draw();
 
+  /**
+   * Update the number of divisions of this keyboard to the specified number.
+   *
+   * @param num_divisions The new number of divisions for this keyboard
+   */
   void UpdateDivisions(size_t num_divisions);
 
  private:
